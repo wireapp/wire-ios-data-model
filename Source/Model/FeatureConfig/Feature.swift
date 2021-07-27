80310df7f37531370e47f603e09c556d45b0054c
KaterinaWire <katerina@wire.com>
2021-07-27 09:56:40 +0200

    Add saveOrRollback

--- Source/Model/FeatureConfig/Feature.swift
+++ Source/Model/FeatureConfig/Feature.swift
@@ -142,6 +142,7 @@ public class Feature: ZMManagedObject {
                 feature.name = name
                 changes(feature)
             }
+            context.saveOrRollback()
         }
     }
 
