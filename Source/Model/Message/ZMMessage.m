41e85665bf08818c296d9f4018dab9a6072d420e
Jacob Persson <jacob.persson@gmail.com>
2021-08-13 15:35:28 +0200

    [Feature] Parse qualified IDs when available (#1204)

--- Source/Model/Message/ZMMessage.m
+++ Source/Model/Message/ZMMessage.m
@@ -294,7 +294,7 @@ NSString * const ZMMessageDecryptionErrorCodeKey = @"decryptionErrorCode";
 - (void)updateWithUpdateEvent:(ZMUpdateEvent *)event forConversation:(ZMConversation *)conversation
 {    
     self.visibleInConversation = conversation;
-    ZMUser *sender = [ZMUser fetchOrCreateWith:event.senderUUID domain:nil in:self.managedObjectContext];
+    ZMUser *sender = [ZMUser fetchOrCreateWith:event.senderUUID domain:event.senderDomain in:self.managedObjectContext];
     if (sender != nil && !sender.isZombieObject && self.managedObjectContext == sender.managedObjectContext) {
         self.sender = sender;
     } else {
@@ -315,7 +315,7 @@ NSString * const ZMMessageDecryptionErrorCodeKey = @"decryptionErrorCode";
         return prefetchResult.conversationsByRemoteIdentifier[conversationUUID];
     }
 
-    return [ZMConversation fetchOrCreateWith:conversationUUID domain:nil in:moc];
+    return [ZMConversation fetchOrCreateWith:conversationUUID domain:event.senderDomain in:moc];
 }
 
 - (void)removeMessageClearingSender:(BOOL)clearingSender
