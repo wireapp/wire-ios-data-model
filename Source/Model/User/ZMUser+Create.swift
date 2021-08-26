4ccba90ff322882781d3b9577c5703c45e75b806
Jacob Persson <jacob.persson@gmail.com>
2021-08-26 11:32:21 +0200

    Delete duplicate merging of users and conversations

--- Source/Model/User/ZMUser+Create.swift
+++ Source/Model/User/ZMUser+Create.swift
@@ -63,21 +63,4 @@ public extension ZMUser {
         }
     }
 
-    // TODO jacob either we delete the duplicate merging or we update it to handle domains
-    @objc static func fetchAndMerge(with remoteIdentifier: UUID, createIfNeeded: Bool, in context: NSManagedObjectContext) -> ZMUser? {
-        // We must only ever call this on the sync context. Otherwise, there's a race condition
-        // where the UI and sync contexts could both insert the same user (same UUID) and we'd end up
-        // having two duplicates of that user, and we'd have a really hard time recovering from that.
-        //
-        assert(!createIfNeeded || context.zm_isSyncContext, "Race condition!")
-        if let result = fetchAndMergeDuplicates(with: remoteIdentifier, in: context) {
-            return result
-        } else if(createIfNeeded) {
-            let user = ZMUser.insertNewObject(in: context)
-            user.remoteIdentifier = remoteIdentifier
-            return user
-        } else {
-            return nil
-        }
-    }
 }
