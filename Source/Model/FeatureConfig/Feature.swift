92bc848bc52ade04a557369a3e37e75a01e53d85
KaterinaWire <katerina@wire.com>
2021-09-07 15:36:10 +0200

    clean up

--- Source/Model/FeatureConfig/Feature.swift
+++ Source/Model/FeatureConfig/Feature.swift
@@ -63,6 +63,7 @@ public class Feature: ZMManagedObject {
                 updateNeedsToNotifyUser(oldData: configData, newData: newValue)
             }
             configData = newValue
+            hasInitialDefault = false
         }
     }
     
