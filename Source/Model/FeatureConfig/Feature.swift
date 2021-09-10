751e7815f4ef730baca76cf702cf6566f40ef902
KaterinaWire <katerina@wire.com>
2021-09-10 08:51:55 +0200

    Indentation fix

--- Source/Model/FeatureConfig/Feature.swift
+++ Source/Model/FeatureConfig/Feature.swift
@@ -148,8 +148,8 @@ public class Feature: ZMManagedObject {
     ///     - changes: A closure to mutate the fetched instance.
 
     public static func updateOrCreate(havingName name: Name,
-                               in context: NSManagedObjectContext,
-                               changes: @escaping (Feature) -> Void) {
+                                      in context: NSManagedObjectContext,
+                                      changes: @escaping (Feature) -> Void) {
 
         // There should be at most one instance per feature, so only allow modifications
         // on a single context to avoid race conditions.
