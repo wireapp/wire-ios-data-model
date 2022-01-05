41791877c5575adcc7b92fe45a7a312f19a10124
KaterinaWire <57407805+KaterinaWire@users.noreply.github.com>
2022-01-05 17:11:34 +0200

    Update Source/ManagedObjectContext/NSManagedObjectContext+ZMKeyValueStore.swift
    Co-authored-by: John Nguyen <28632506+johnxnguyen@users.noreply.github.com>

--- Source/ManagedObjectContext/NSManagedObjectContext+ZMKeyValueStore.swift
+++ Source/ManagedObjectContext/NSManagedObjectContext+ZMKeyValueStore.swift
@@ -18,7 +18,8 @@
 
 import Foundation
 
-@objc(ZMKeyValueStore) public protocol KeyValueStore : NSObjectProtocol {
+@objc(ZMKeyValueStore)
+public protocol KeyValueStore: NSObjectProtocol {
 
     func store(value: PersistableInMetadata?, key: String)
     func storedValue(key: String) -> Any?
