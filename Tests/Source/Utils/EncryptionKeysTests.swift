812b4744eaec5fcac69fe92d047c3ec0b8c4f5af
Bill Chan <billypchan@gmail.com>
2022-01-06 17:16:08 +0100

    Update Tests/Source/Utils/EncryptionKeysTests.swift

--- Tests/Source/Utils/EncryptionKeysTests.swift
+++ Tests/Source/Utils/EncryptionKeysTests.swift
@@ -74,11 +74,11 @@ class EncryptionKeysTests: XCTestCase {
 
     func testThatEncryptionKeysAreSuccessfullyFetched() throws {
         // given
-//#if targetEnvironment(simulator)
-//        if #available(iOS 15, *) {
-//            XCTExpectFailure("Expect to fail on iOS 15 simulator. ref: https://wearezeta.atlassian.net/browse/SQCORE-1188")
-//        }
-//#endif
+#if targetEnvironment(simulator)
+        if #available(iOS 15, *) && swift(>=5.4) {
+            XCTExpectFailure("Expect to fail on iOS 15 simulator. ref: https://wearezeta.atlassian.net/browse/SQCORE-1188")
+        }
+#endif
         _ = try EncryptionKeys.createKeys(for: account)
 
         // then
