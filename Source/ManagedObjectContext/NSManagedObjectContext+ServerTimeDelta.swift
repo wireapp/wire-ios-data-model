0590b690cebb64a73692d7d1737bcd7433f91230
KaterinaWire <katerina@wire.com>
2022-03-07 09:21:19 +0100

    Fix SwiftLint issues

--- Source/ManagedObjectContext/NSManagedObjectContext+ServerTimeDelta.swift
+++ Source/ManagedObjectContext/NSManagedObjectContext+ServerTimeDelta.swift
@@ -19,23 +19,22 @@
 import Foundation
 
 public extension NSManagedObjectContext {
-    
+
     private static let ServerTimeDeltaKey = "ServerTimeDeltaKey"
-    
+
     @objc
     var serverTimeDelta: TimeInterval {
-        
+
         get {
             precondition(zm_isSyncContext, "serverTimeDelta can only be accessed on the sync context")
             return userInfo[NSManagedObjectContext.ServerTimeDeltaKey] as? TimeInterval ?? 0
         }
-        
+
         set {
             precondition(zm_isSyncContext, "serverTimeDelta can only be accessed on the sync context")
             userInfo[NSManagedObjectContext.ServerTimeDeltaKey] = newValue
         }
-        
-    }
-        
-}
 
+    }
+
+}
