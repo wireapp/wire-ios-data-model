f6c532d156d6f67aadf418a245a82129c852e154
John Nguyen <28632506+johnxnguyen@users.noreply.github.com>
2022-05-26 16:54:40 +0200

    add tests

--- Source/Model/FeatureConfig/MLS/Feature.MLS.swift
+++ Source/Model/FeatureConfig/MLS/Feature.MLS.swift
@@ -44,7 +44,7 @@ public extension Feature {
         // WARNING: This config is encoded and stored in the database, so any changes
         // to it will require some migration code.
 
-        public struct Config: Codable {
+        public struct Config: Codable, Equatable {
 
             /// The ids of users who have the option to create new MLS groups.
 
--- Tests/Source/Model/FeatureConfiguration/FeatureServiceTests.swift
+++ Tests/Source/Model/FeatureConfiguration/FeatureServiceTests.swift
@@ -21,6 +21,132 @@ import XCTest
 
 class FeatureServiceTests: ZMBaseManagedObjectTest {
 
+    override func setUp() {
+        super.setUp()
+        deleteFeatureIfNeeded(name: .mls)
+    }
+
+    func deleteFeatureIfNeeded(name: Feature.Name) {
+        syncMOC.performAndWait {
+            if let feature = Feature.fetch(name: name, context: self.syncMOC) {
+                self.syncMOC.delete(feature)
+            }
+        }
+    }
+
+    // MARK: - MLS
+
+    func testThatItFetchesMLS() {
+        syncMOC.performGroupedBlock {
+            // Given
+            let sut = FeatureService(context: self.syncMOC)
+
+            let config = Feature.MLS.Config(
+                protocolToggleUsers: [.create()],
+                defaultProtocol: .mls,
+                allowedCipherSuites: [.MLS_128_DHKEMP256_AES128GCM_SHA256_P256],
+                defaultCipherSuite: .MLS_256_DHKEMX448_AES256GCM_SHA512_Ed448
+            )
+
+            Feature.updateOrCreate(havingName: .mls, in: self.syncMOC) { feature in
+                feature.status = .enabled
+                feature.config = try! JSONEncoder().encode(config)
+            }
+
+            // When
+            let result = sut.fetchMLS()
+
+            // Then
+            XCTAssertEqual(result.status, .enabled)
+            XCTAssertEqual(result.config, config)
+        }
+
+        XCTAssertTrue(waitForAllGroupsToBeEmpty(withTimeout: 0.5))
+    }
+
+    func testThatItFetchesMLS_ItReturnsADefaultConfigWhenConfigDoesNotExist() {
+        syncMOC.performGroupedBlock {
+            // Given
+            let sut = FeatureService(context: self.syncMOC)
+
+            Feature.updateOrCreate(havingName: .mls, in: self.syncMOC) { feature in
+                feature.status = .enabled
+                feature.config = nil
+            }
+
+            // When
+            let result = sut.fetchMLS()
+
+            // Then
+            XCTAssertEqual(result.status, .disabled)
+            XCTAssertEqual(result.config, .init())
+        }
+
+        XCTAssertTrue(waitForAllGroupsToBeEmpty(withTimeout: 0.5))
+    }
+
+    func testThatItFetchesMLS_ItReturnsADefaultConfigWhenObjectDoesNotExist() {
+        syncMOC.performGroupedBlock {
+            // Given
+            let sut = FeatureService(context: self.syncMOC)
+            XCTAssertNil(Feature.fetch(name: .mls, context: self.syncMOC))
+
+            // When
+            let result = sut.fetchMLS()
+
+            // Then
+            XCTAssertEqual(result.status, .disabled)
+            XCTAssertEqual(result.config, .init())
+        }
+
+        XCTAssertTrue(waitForAllGroupsToBeEmpty(withTimeout: 0.5))
+    }
+
+    func testThatItStoresMLS() {
+        syncMOC.performGroupedBlock {
+            // Given
+            let sut = FeatureService(context: self.syncMOC)
+
+            let config = Feature.MLS.Config(
+                protocolToggleUsers: [.create()],
+                defaultProtocol: .mls,
+                allowedCipherSuites: [.MLS_128_DHKEMP256_AES128GCM_SHA256_P256],
+                defaultCipherSuite: .MLS_256_DHKEMX448_AES256GCM_SHA512_Ed448
+            )
+
+            let mls = Feature.MLS(
+                status: .enabled,
+                config: config
+            )
+
+            XCTAssertNil(Feature.fetch(name: .mls, context: self.syncMOC))
+
+            // When
+            sut.storeMLS(mls)
+
+            // Then
+            guard let feature = Feature.fetch(name: .mls, context: self.syncMOC) else {
+                XCTFail("feature not found")
+                return
+            }
+
+            guard let configData = feature.config else {
+                XCTFail("expected config data")
+                return
+            }
+
+            guard let featureConfig = configData.decode(as: Feature.MLS.Config.self) else {
+                XCTFail("failed to decode config data")
+                return
+            }
+
+            XCTAssertEqual(feature.status, .enabled)
+            XCTAssertEqual(featureConfig, config)
+        }
+
+        XCTAssertTrue(waitForAllGroupsToBeEmpty(withTimeout: 0.5))
+    }
+
     func testThatItStoresAppLockFeature() {
         // Given
         let sut = FeatureService(context: syncMOC)
@@ -74,6 +200,14 @@ private extension Feature.AppLock {
 
     var configData: Data {
         return try! JSONEncoder().encode(config)
+    }
+
+}
+
+private extension Data {
+
+    func decode<T: Decodable>(as type: T.Type) -> T? {
+        return try? JSONDecoder().decode(type, from: self)
     }
 
 }
