f569dc6e84ced7326b6a068f5a509b2ffee3a21b
John Nguyen <28632506+johnxnguyen@users.noreply.github.com>
2022-07-11 14:54:15 +0200

    wip create group

--- MLS/MLSController.swift
+++ MLS/MLSController.swift
@@ -64,4 +64,187 @@ public final class MLSController {
         context.saveOrRollback()
     }
 
+    @available(iOS 13, *)
+    func createGroup(for conversation: ZMConversation) {
+        Task {
+            guard let context = context else { return }
+            guard let groupID = conversation.mlsGroupID else { return }
+
+            let allKeyPackages = claimKeyPackages(
+                for: Array(conversation.localParticipants),
+                in: context
+            )
+
+            // Collect all the key packages.
+            var buffer = [KeyPackage]()
+
+            do {
+                for try await keyPackages in allKeyPackages {
+                    buffer.append(contentsOf: keyPackages)
+                }
+            } catch let error {
+                fatalError("Failed to claim key packages: \(String(describing: error))")
+            }
+
+            // Deafult config for the group.
+            let config = ConversationConfiguration(
+                extraMembers: [],
+                admins: [],
+                ciphersuite: .mls128Dhkemx25519Aes128gcmSha256Ed25519,
+                keyRotationSpan: nil
+            )
+
+            var messagesToPost: MemberAddedMessages?
+
+            do {
+                messagesToPost = try coreCrypto.wire_createConversation(
+                    conversationId: groupID.bytes,
+                    config: config
+                )
+
+                // TODO: check if messagesToPost is nil after creating the conversation.
+
+            } catch let error {
+                fatalError("Failed to create mls group: \(String(describing: error))")
+            }
+
+            do {
+                messagesToPost = try coreCrypto.wire_addClientsToConversation(
+                    conversationId: groupID.bytes,
+                    clients: buffer.map(Invitee.init(from:))
+                )
+
+                // TODO: post messagesToPost.message
+                // TODO: post messagesToPost.welcom
+
+            } catch let error {
+                fatalError("Failed to add clients to the conversation: \(String(describing: error))")
+            }
+        }
+    }
+
+    @available(iOS 13, *)
+    private func claimKeyPackages(
+        for users: [ZMUser],
+        in context: NSManagedObjectContext
+    ) -> AsyncThrowingStream<([KeyPackage]), Error> {
+        var index = 0
+
+        return AsyncThrowingStream {
+            guard let user = users.element(atIndex: index) else { return nil }
+
+            index += 1
+
+            var action = ClaimMLSKeyPackageAction(
+                domain: user.domain,
+                userId: user.remoteIdentifier,
+                excludedSelfClientId: user.isSelfUser ? user.selfClient()?.remoteIdentifier : nil
+            )
+
+            return try await action.perform(in: context.notificationContext)
+        }
+    }
+
+}
+
+private extension EntityAction {
+
+    @available(*, renamed: "perform(in:)")
+    mutating func perform(
+        in context: NotificationContext,
+        resultHandler: @escaping ResultHandler
+    ) {
+        self.resultHandler = resultHandler
+        send(in: context)
+    }
+
+    @available(iOS 13, *)
+    mutating func perform(in context: NotificationContext) async throws -> Result {
+        return try await withCheckedThrowingContinuation { continuation in
+            perform(in: context, resultHandler: continuation.resume(with:))
+        }
+    }
+
+}
+
+extension ZMUser {
+
+    var mlsClientID: String {
+        return ""
+    }
+
+}
+
+extension Invitee {
+
+    init(from keyPackage: KeyPackage) {
+        let id = MLSClientID(
+            userID: keyPackage.userID.uuidString,
+            clientID: keyPackage.client,
+            domain: keyPackage.domain
+        )
+
+        guard
+            let idData = id.string.utf8Data,
+            let keyPackageData = Data(base64Encoded: keyPackage.keyPackage)
+        else {
+            fatalError("Couldn't create Invitee from key package: \(keyPackage)")
+        }
+
+        self.init(
+            id: idData.bytes,
+            kp: keyPackageData.bytes
+        )
+    }
+
+}
+
+private extension String {
+
+    var utf8Data: Data? {
+        return data(using: .utf8)
+    }
+
+    var base64DecodedData: Data? {
+        return Data(base64Encoded: self)
+    }
+
+
+}
+
+struct MLSClientID: Equatable {
+
+    private let userID: String
+    private let clientID: String
+    private let domain: String
+
+    let string: String
+
+    init?(userClient: UserClient) {
+        guard
+            let userID = userClient.user?.remoteIdentifier.uuidString,
+            let clientID = userClient.remoteIdentifier,
+            let domain = userClient.user?.domain ?? APIVersion.domain
+        else {
+            return nil
+        }
+
+        self.init(
+            userID: userID,
+            clientID: clientID,
+            domain: domain
+        )
+    }
+
+    init(
+        userID: String,
+        clientID: String,
+        domain: String
+    ) {
+        self.userID = userID.lowercased()
+        self.clientID = clientID.lowercased()
+        self.domain = domain.lowercased()
+        self.string = "\(self.userID):\(self.clientID)@\(self.domain)"
+    }
+
 }
